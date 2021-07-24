1a1cecbef930cacaf3ddfd20b39b26323a44c947
Rafael Penna <rafaelgpenna@gmail.com>
2021-07-23 21:49:53 -0300

    test: add validation to CPF containing dashes and dots

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -13,37 +13,44 @@ def test_should_validate_correct_cpf(validator):
     assert validator.validate_cpf('12243951070')
 
 def test_should_deny_incorrect_cpf(validator):
-    assert validator.validate_cpf('46499458045')
-    assert validator.validate_cpf('31195615093')
-    assert validator.validate_cpf('40377045082')
-    assert validator.validate_cpf('03239420040')
-    assert validator.validate_cpf('20914218002')
+    assert validator.validate_cpf('46499458045') == False
+    assert validator.validate_cpf('31195615093') == False
+    assert validator.validate_cpf('40377045082') == False
+    assert validator.validate_cpf('03239420040') == False
+    assert validator.validate_cpf('20914218002') == False
 
 def test_should_deny_cpf_with_wrong_length(validator):
-    assert validator.validate_cpf('3029184005')
-    assert validator.validate_cpf('302918400')
-    assert validator.validate_cpf('30291840')
-    assert validator.validate_cpf('3029184')
-    assert validator.validate_cpf('302918')
-    assert validator.validate_cpf('30291')
-    assert validator.validate_cpf('3029')
-    assert validator.validate_cpf('302')
-    assert validator.validate_cpf('30')
-    assert validator.validate_cpf('3')
-    assert validator.validate_cpf('')
-    assert validator.validate_cpf('302918400511')
-    assert validator.validate_cpf('3029184005123')
-    assert validator.validate_cpf('30291840051234')
-    assert validator.validate_cpf('302918400512345')
-    assert validator.validate_cpf('3029184005123456')
-    assert validator.validate_cpf('30291840051234567')
-    assert validator.validate_cpf('302918400512345678')
-    assert validator.validate_cpf('3029184005123456789')
-    assert validator.validate_cpf('30291840051234567890')
+    assert validator.validate_cpf('3029184005') == False
+    assert validator.validate_cpf('302918400') == False
+    assert validator.validate_cpf('30291840') == False
+    assert validator.validate_cpf('3029184') == False
+    assert validator.validate_cpf('302918') == False
+    assert validator.validate_cpf('30291') == False
+    assert validator.validate_cpf('3029') == False
+    assert validator.validate_cpf('302') == False
+    assert validator.validate_cpf('30') == False
+    assert validator.validate_cpf('3') == False
+    assert validator.validate_cpf('') == False
+    assert validator.validate_cpf('302918400511') == False
+    assert validator.validate_cpf('3029184005123') == False
+    assert validator.validate_cpf('30291840051234') == False
+    assert validator.validate_cpf('302918400512345') == False
+    assert validator.validate_cpf('3029184005123456') == False
+    assert validator.validate_cpf('30291840051234567') == False
+    assert validator.validate_cpf('302918400512345678') == False
+    assert validator.validate_cpf('3029184005123456789') == False
+    assert validator.validate_cpf('30291840051234567890') == False
 
 def test_should_deny_cpf_with_non_numeric_chars(validator):
-    assert validator.validate_cpf('302918A0051')
-    assert validator.validate_cpf('E0291840051')
-    assert validator.validate_cpf('3029184005I')
-    assert validator.validate_cpf('30291840O51')
-    assert validator.validate_cpf('30291$40O51')
+    assert validator.validate_cpf('302918A0051') == False
+    assert validator.validate_cpf('E0291840051') == False
+    assert validator.validate_cpf('3029184005I') == False
+    assert validator.validate_cpf('30291840O51') == False
+    assert validator.validate_cpf('30291$40O51') == False
+
+def test_should_validate_cpf_containing_dashes_and_dots(validator):
+    assert validator.validate_cpf('157.823.860-96')
+    assert validator.validate_cpf('101.203.370-88')
+    assert validator.validate_cpf('046.960.300-32')
+    assert validator.validate_cpf('858.664.030-13')
+    assert validator.validate_cpf('753.394.110-10')
