820f85a1938ee35f3421521dcaafad6b3cea6fc6
Rafael Penna <rafaelgpenna@gmail.com>
2021-07-23 22:55:08 -0300

    feat: add cpf validator

---
+++ src/validator.py
@@ -0,0 +1,35 @@
+import re
+
+class Validator:
+    """Class for validating data
+    """
+    def validate_cpf(self, cpf: str) -> bool:
+        """Validate cpf string, containing no special chars
+
+        Args:
+            cpf (str): CPF about to be validated
+
+        Returns:
+            bool: True if CPF is valid
+        """
+        cpf = cpf.replace('-', '').replace('.', '')
+        if len(cpf) != 11:
+            return False
+        re_pattern = re.compile(r'^\d{11}$')
+        if not re.fullmatch(re_pattern, cpf):
+            return False
+        soma = 0
+        for i in range(0, 9):
+            soma += int(cpf[i]) * (10 - i)
+        digito1 = 11 - (soma % 11)
+        if digito1 >= 10:
+            digito1 = 0
+        if str(digito1) != cpf[-2]:
+            return False
+        soma = 0
+        for i in range(0, 10):
+            soma += int(cpf[i]) * (11 - i)
+        digito2 = 11 - (soma % 11)
+        if digito2 >= 10:
+            digito2 = 0
+        return str(digito2) == cpf[-1]
