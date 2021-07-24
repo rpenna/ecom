961669493b2f91a338bb5c8a6c07682ba2616186
Rafael Penna <rafaelgpenna@gmail.com>
2021-07-24 16:11:55 -0300

    refactor: cpf validation code refactored

--- src/validator.py
+++ src/validator.py
@@ -1,12 +1,56 @@
 import re
 
+CPF_LENGTH = 11
+
 class Validator:
     """Class for validating data
     """
 
-    #def __remove_dashes_dots_and_whitespaces(self, cpf: str) -> str:
-    #    return cpf.replace('-', '').replace('.', '').strip()
+    def __remove_dashes_dots_and_whitespaces(self, cpf: str) -> str:
+        """Remove all dashes and dots from the CPF, then trims the content
+
+        Args:
+            cpf (str): CPF
+
+        Returns:
+            str: trimmed CPF
+        """
+        return cpf.replace('-', '').replace('.', '').strip()
+
+    def __validate_length(self, content: str, length: int) -> bool:
+        """Check if the string contains exactly the length informed
+
+        Args:
+            content (str): string to be evaluated
+            length (int): the expected length
+
+        Returns:
+            bool: True if the string has the expected length
+        """
+        return len(content) == length
      
+    def __has_only_numbers(self, content: str) -> bool:
+        """Check if the string received contains only numeric digits
+
+        Args:
+            content (str): string to be evaluated
+
+        Returns:
+            bool: True if contains only numeric digits
+        """
+        re_pattern = re.compile(r'^\d+$')
+        return re.fullmatch(re_pattern, content) is not None
+
+    def __calculate_checking_digit(self, cpf: str) -> str:
+        digits_sum = 0
+        length = len(cpf)
+        for i in range(0, length):
+            digits_sum += int(cpf[i]) * (length + 1 - i)
+        checking_digit = CPF_LENGTH - (digits_sum % CPF_LENGTH)
+        if checking_digit >= 10:
+            return '0'
+        return str(checking_digit)
+
     def validate_cpf(self, cpf: str) -> bool:
         """Validate cpf string, containing no special chars
 
@@ -16,24 +60,13 @@ class Validator:
         Returns:
             bool: True if CPF is valid
         """
-        cpf = cpf.replace('-', '').replace('.', '').strip()
-        if len(cpf) != 11:
+        trimmed_cpf = self.__remove_dashes_dots_and_whitespaces(cpf)
+        if not self.__validate_length(trimmed_cpf, CPF_LENGTH):
             return False
-        re_pattern = re.compile(r'^\d{11}$')
-        if not re.fullmatch(re_pattern, cpf):
+        if not self.__has_only_numbers(trimmed_cpf):
             return False
-        soma = 0
-        for i in range(0, 9):
-            soma += int(cpf[i]) * (10 - i)
-        digito1 = 11 - (soma % 11)
-        if digito1 >= 10:
-            digito1 = 0
-        if str(digito1) != cpf[-2]:
+        first_digit = self.__calculate_checking_digit(trimmed_cpf[:-2])
+        if first_digit != trimmed_cpf[-2]:
             return False
-        soma = 0
-        for i in range(0, 10):
-            soma += int(cpf[i]) * (11 - i)
-        digito2 = 11 - (soma % 11)
-        if digito2 >= 10:
-            digito2 = 0
-        return str(digito2) == cpf[-1]
+        second_digit = self.__calculate_checking_digit(trimmed_cpf[:-1])
+        return second_digit == trimmed_cpf[-1]
