5f0cc1917f4621a7c7e1fba6f32484ae058aaf4d
Rafael Penna <rafaelgpenna@gmail.com>
2021-07-30 20:41:17 -0300

    test: add test for PlaceOrder class

---
+++ tests/test_place_order.py
@@ -0,0 +1,34 @@
+# TODO: create tests for place order use case. They must not know anything about the entities.
+import pytest
+from decimal import Decimal
+
+from ..src.place_order import PlaceOrder
+
+def test_should_place_order_containing_three_products():
+    order = {
+        'cart': [
+            {
+                'description': 'book',
+                'price': 19.9,
+                'quantity': 5
+            },
+            {
+                'description': 'pff2 mask',
+                'price': 2.8,
+                'quantity': 30
+            },
+            {
+                'description': 'vacuum cleaner',
+                'price': 227.99,
+                'quantity': 1
+            }
+        ],
+        'coupon': {
+            'code': '15off',
+            'discount': 15
+        },
+        'cpf': '01234567890'
+    }
+    place_order = PlaceOrder()
+    order_summary = place_order.execute(order)
+    assert order_summary['total'] == Decimal('411.49')
