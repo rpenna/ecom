c32d45b22289cd7040d138e45c09c0fc8bd07164
Rafael Penna <rafaelgpenna@gmail.com>
2021-07-29 21:55:42 -0300

    test: adapted for refactored code

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,8 +1,9 @@
 import pytest
+from decimal import Decimal
 
 from ..src.order import Order
+from ..src.coupon import Coupon
 from ..src.invalid_cpf import InvalidCpf
-from ..src.product_unavailable import ProductUnavailable
 
 @pytest.fixture
 def order():
@@ -16,42 +17,15 @@ def order_three_products():
     order.add_to_cart('vacuum cleaner', 227.99, 1)
     return order
 
-@pytest.fixture
-def invalid_cpf_order():
-    return Order('30291840050')
 
 def test_should_close_order_with_three_products(order_three_products):
-    summary = order_three_products.close_order()
-    assert summary['total'] == 303.89
+    assert order_three_products.get_total() == Decimal('303.89')
 
 def test_should_apply_discount(order_three_products):
-    order_three_products.add_discount_coupon('10offCoupon')
-    summary = order_three_products.close_order()
-    assert summary['total'] == 273.5
+    coupon = Coupon('10offCoupon', 10)
+    order_three_products.add_discount_coupon(coupon)
+    assert order_three_products.get_total() == Decimal('273.50')
 
-def test_should_deny_close_order_with_invalid_cpf(invalid_cpf_order):
+def test_should_deny_close_order_with_invalid_cpf():
     with pytest.raises(InvalidCpf):
-        invalid_cpf_order.add_to_cart('microwave', 459.99, 1)
-        invalid_cpf_order.close_order()
-
-def test_should_deny_adding_unexistent_product_to_cart(order):
-    with pytest.raises(ProductUnavailable):
-        order.add_to_cart(
-            'unexistent_product',
-            1,
-            1
-        )
-
-def test_should_deny_adding_unavailable_product_to_cart(order):
-    with pytest.raises(ProductUnavailable):
-        order.add_to_cart(
-            'microwave',
-            459.99,
-            1000
-        )
-    with pytest.raises(ProductUnavailable):
-        order.add_to_cart(
-            'queen album',
-            50.0,
-            1
-        )
+        order = Order('30291840050')
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,64 +1,133 @@
 import pytest
-from ..src.validator import Validator
+from ..src.cpf import Cpf
+from ..src.invalid_cpf import InvalidCpf
 
-@pytest.fixture
-def validator():
-    return Validator()
+valid_cpf = (
+    '30291840051',
+    '47465913004',
+    '03574151080',
+    '52632277019',
+    '12243951070',
+)
 
-def test_should_validate_correct_cpf(validator):
-    assert validator.validate_cpf('30291840051')
-    assert validator.validate_cpf('47465913004')
-    assert validator.validate_cpf('03574151080')
-    assert validator.validate_cpf('52632277019')
-    assert validator.validate_cpf('12243951070')
+invalid_cpf = (
+    '46499458045',
+    '31195615093',
+    '40377045082',
+    '03239420040',
+    '20914218002',
+)
 
-def test_should_deny_incorrect_cpf(validator):
-    assert validator.validate_cpf('46499458045') == False
-    assert validator.validate_cpf('31195615093') == False
-    assert validator.validate_cpf('40377045082') == False
-    assert validator.validate_cpf('03239420040') == False
-    assert validator.validate_cpf('20914218002') == False
+wrong_length_cpf = (
+    '3029184005',
+    '302918400',
+    '30291840',
+    '3029184',
+    '302918',
+    '30291',
+    '3029',
+    '302',
+    '30',
+    '3',
+    '',
+    '302918400511',
+    '3029184005123',
+    '30291840051234',
+    '302918400512345',
+    '3029184005123456',
+    '30291840051234567',
+    '302918400512345678',
+    '3029184005123456789',
+    '30291840051234567890',
+)
 
-def test_should_deny_cpf_with_wrong_length(validator):
-    assert validator.validate_cpf('3029184005') == False
-    assert validator.validate_cpf('302918400') == False
-    assert validator.validate_cpf('30291840') == False
-    assert validator.validate_cpf('3029184') == False
-    assert validator.validate_cpf('302918') == False
-    assert validator.validate_cpf('30291') == False
-    assert validator.validate_cpf('3029') == False
-    assert validator.validate_cpf('302') == False
-    assert validator.validate_cpf('30') == False
-    assert validator.validate_cpf('3') == False
-    assert validator.validate_cpf('') == False
-    assert validator.validate_cpf('302918400511') == False
-    assert validator.validate_cpf('3029184005123') == False
-    assert validator.validate_cpf('30291840051234') == False
-    assert validator.validate_cpf('302918400512345') == False
-    assert validator.validate_cpf('3029184005123456') == False
-    assert validator.validate_cpf('30291840051234567') == False
-    assert validator.validate_cpf('302918400512345678') == False
-    assert validator.validate_cpf('3029184005123456789') == False
-    assert validator.validate_cpf('30291840051234567890') == False
+non_numeric_chars_cpf = (
+    '302918A0051',
+    'E0291840051',
+    '3029184005I',
+    '30291840O51',
+    '30291$40O51',
+)
 
-def test_should_deny_cpf_with_non_numeric_chars(validator):
-    assert validator.validate_cpf('302918A0051') == False
-    assert validator.validate_cpf('E0291840051') == False
-    assert validator.validate_cpf('3029184005I') == False
-    assert validator.validate_cpf('30291840O51') == False
-    assert validator.validate_cpf('30291$40O51') == False
+cpf_with_dashes_and_dots = (
+    '753.394.110-10',
+    '157.823.860-96',
+    '101.203.370-88',
+    '046.960.300-32',
+    '858.664.030-13',
+)
 
-def test_should_validate_cpf_containing_dashes_and_dots(validator):
-    assert validator.validate_cpf('157.823.860-96')
-    assert validator.validate_cpf('101.203.370-88')
-    assert validator.validate_cpf('046.960.300-32')
-    assert validator.validate_cpf('858.664.030-13')
-    assert validator.validate_cpf('753.394.110-10')
+trailing_whitespaces = (
+    '753.394.110-10 ',
+    '753.394.110-10  ',
+)
 
-def test_should_validate_cpf_containing_trailing_whitespaces(validator):
-    assert validator.validate_cpf('753.394.110-10 ')
-    assert validator.validate_cpf('753.394.110-10  ')
+leading_whitespaces = (
+    '  753.394.110-10',
+    ' 753.394.110-10',
+)
 
-def test_should_validate_cpf_containing_leading_whitespaces(validator):
-    assert validator.validate_cpf('  753.394.110-10')
-    assert validator.validate_cpf(' 753.394.110-10')
+
+@pytest.mark.parametrize(
+    'valid_cpf',
+    valid_cpf
+)
+def test_should_validate_correct_cpf(valid_cpf):
+    try:
+        Cpf(valid_cpf)
+    except InvalidCpf:
+        pytest.fail('Unexpected InvalidCpf raised')
+
+@pytest.mark.parametrize(
+    'invalid_cpf',
+    invalid_cpf
+)
+def test_should_deny_incorrect_cpf(invalid_cpf):
+    with pytest.raises(InvalidCpf):
+        Cpf(invalid_cpf)
+
+@pytest.mark.parametrize(
+    'wrong_length_cpf',
+    wrong_length_cpf
+)
+def test_should_deny_cpf_with_wrong_length(wrong_length_cpf):
+    with pytest.raises(InvalidCpf):
+        Cpf(wrong_length_cpf)
+
+@pytest.mark.parametrize(
+    'non_numeric_chars_cpf',
+    non_numeric_chars_cpf
+)
+def test_should_deny_cpf_with_non_numeric_chars(non_numeric_chars_cpf):
+    with pytest.raises(InvalidCpf):
+        Cpf(non_numeric_chars_cpf)
+
+@pytest.mark.parametrize(
+    'cpf_with_dashes_and_dots',
+    cpf_with_dashes_and_dots
+)
+def test_should_validate_cpf_containing_dashes_and_dots(cpf_with_dashes_and_dots):
+    try:
+        Cpf(cpf_with_dashes_and_dots)
+    except InvalidCpf:
+        pytest.fail('Unexpected InvalidCpf raised')
+
+@pytest.mark.parametrize(
+    'trailing_whitespaces',
+    trailing_whitespaces
+)
+def test_should_validate_cpf_containing_trailing_whitespaces(trailing_whitespaces):
+    try:
+        Cpf(trailing_whitespaces)
+    except InvalidCpf:
+        pytest.fail('Unexpected InvalidCpf raised')
+
+@pytest.mark.parametrize(
+    'leading_whitespaces',
+    leading_whitespaces
+)
+def test_should_validate_cpf_containing_leading_whitespaces(leading_whitespaces):
+    try:
+        Cpf(leading_whitespaces)
+    except InvalidCpf:
+        pytest.fail('Unexpected InvalidCpf raised')
