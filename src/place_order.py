a488f8eaa067465eb0d0a469a81fc5c3a8b321f2
Rafael Penna <rafaelgpenna@gmail.com>
2021-07-30 21:52:29 -0300

    refactor: PlaceOrder class refactored

--- src/place_order.py
+++ src/place_order.py
@@ -2,19 +2,53 @@ from .coupon import Coupon
 from .order import Order
 
 class PlaceOrder:
-    def execute(self, data: dict) -> dict:
-        order = Order(data.get('cpf', ''))
-        for product in data.get('cart', []):
-            order.add_to_cart(
+    def __init__(self):
+        self.__order = None
+
+    def __add_products_to_cart(self, products: list) -> None:
+        """Add all products listed to the cart
+
+        Args:
+            products (list): products received via DTO
+
+        Returns:
+            None: no return expected
+        """
+        for product in products:
+            self.__order.add_to_cart(
                 product.get('description'),
                 product.get('price'),
                 product.get('quantity')
             )
+
+    def __apply_discount(self, coupon: dict) -> None:
+        """Apply the discount informed by the coupon
+
+        Args:
+            coupon (dict): Coupon received from DTO
+
+        Returns:
+            None: no return expected
+        """
+        code = coupon.get('code')
+        discount = coupon.get('discount')
+        coupon = Coupon(code, discount)
+        self.__order.add_discount_coupon(coupon)
+
+    def execute(self, data: dict) -> dict:
+        """Place order according to the DTO received
+
+        Args:
+            data (dict): DTO of the order
+
+        Returns:
+            dict: Same content as DTO, but including the 'total' key, which 
+            informs the total amount of the order.
+        """
+        self.__order = Order(data.get('cpf', ''))
+        self.__add_products_to_cart(data.get('cart', []))
         if 'coupon' in data:
-            code = data['coupon'].get('code')
-            discount = data['coupon'].get('discount')
-            coupon = Coupon(code, discount)
-            order.add_discount_coupon(coupon)
+            self.__apply_discount(data['coupon'])
         summary = data
-        summary['total'] = order.get_total()
+        summary['total'] = self.__order.get_total()
         return summary
