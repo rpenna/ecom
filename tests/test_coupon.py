4912140321c618c4b6f89e97788bb3e07214bf38
Rafael Penna <rafaelgpenna@gmail.com>
2021-08-05 20:27:48 -0300

    test: add coupon tests

---
+++ tests/test_coupon.py
@@ -0,0 +1,14 @@
+import pytest
+from datetime import datetime, timedelta
+
+from ..src.coupon import Coupon
+
+def test_expired_coupon_should_be_expired():
+    expired_date = datetime.now() - timedelta(days=1)
+    coupon = Coupon('expired_coupon', 15, expired_date)
+    assert coupon.is_expired()
+
+def test_valid_coupon_should_not_be_expired():
+    not_expired_date = datetime.now() + timedelta(days=1)
+    coupon = Coupon('not_expired_coupon', 10, not_expired_date)
+    assert coupon.is_expired() == False
