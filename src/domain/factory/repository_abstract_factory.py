f73b825e09366de814b408a67ccfbc41ee7e9bc3
Rafael Penna <rafaelgpenna@gmail.com>
2022-02-09 21:07:04 -0300

    refact: add new abstract method to RepositoryAbstractFactory

--- src/domain/factory/repository_abstract_factory.py
+++ src/domain/factory/repository_abstract_factory.py
@@ -3,6 +3,8 @@ from ..repository.order_repository import OrderRepository
 from ..repository.product_repository import ProductRepository
 from ..repository.coupon_repository import CouponRepository
 from ..repository.taxes_repository import TaxesRepository
+from ..repository.stock_repository import StockRepository
+
 
 class RepositoryAbstractFactory(metaclass=abc.ABCMeta):
     def make_order_repository(self) -> OrderRepository:
@@ -10,9 +12,12 @@ class RepositoryAbstractFactory(metaclass=abc.ABCMeta):
 
     def make_product_repository(self) -> ProductRepository:
         raise NotImplementedError
-    
+
     def make_coupon_repository(self) -> CouponRepository:
         raise NotImplementedError
-    
+
     def make_taxes_repository(self) -> TaxesRepository:
+        raise NotImplementedError
+
+    def make_stock_repository(self) -> StockRepository:
         raise NotImplementedError
