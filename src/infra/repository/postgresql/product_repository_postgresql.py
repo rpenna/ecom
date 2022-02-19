257315247d1423c10afa8d476fa59f45147a1416
Rafael Penna <rafaelgpenna@gmail.com>
2022-02-19 16:05:06 -0300

    refact: create postgresql product repository

--- src/infra/repository/postgresql/product_repository_postgresql.py
+++ src/infra/repository/postgresql/product_repository_postgresql.py
@@ -1,5 +1,35 @@
+from inspect import Parameter
+from ...database.postgresql_connection import PostgresConnection
+from ....domain.entity.product import Product
+from ....domain.exception.product_not_found import ProductNotFound
 from ....domain.repository.product_repository import ProductRepository
 
 
 class ProductRepositoryPostgresql(ProductRepository):
-    ...
+    def __init__(self, conn: PostgresConnection):
+        self.__conn = conn
+
+    def get_by_id(self, id: str) -> Product:
+        query = """
+            select 
+                id,
+                description,
+                price,
+                height,
+                width,
+                depth,
+                weight
+            from ecom.products
+            where id = %s
+        """
+        parameters = (id,)
+        product = self.__conn.query_one(query, parameters)
+        if product is None:
+            raise ProductNotFound
+        infos = {
+            "height": product["height"],
+            "width": product["width"],
+            "depth": product["depth"],
+            "weight": product["weight"],
+        }
+        return Product(product["id"], product["description"], product["price"], infos)
